921a111a4a21fffdc5600426179cbebd95df1e6f
CPM <cpm.chuck@gmail.com>
2017-10-22 02:48:15 +0800

    ch1 - complete

---
+++ ch1-review/src/basicType/Main.java
@@ -0,0 +1,33 @@
+package basicType;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//integer(default is int)
+		byte i1 = 1;
+		short i2 = 2;
+		int i3 = 4;
+		
+		//8l or 8L
+		long i4 = 8l;
+		
+		//floating point(default is double)
+		
+		//32.0f or 32.0F
+		float f1 = 32.0f;
+		double f2 = 64.0;
+		
+		//textual
+		char t1 = '\uffff';
+		
+		//logical
+		boolean l1 = true;
+		
+		System.out.println(Integer.MIN_VALUE);
+		System.out.println(Integer.MAX_VALUE);
+
+		System.out.println(Float.MIN_VALUE);
+		System.out.println(Float.MAX_VALUE);
+	}
+
+}
