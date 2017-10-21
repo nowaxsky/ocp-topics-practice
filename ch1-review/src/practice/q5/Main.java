898ac91d6ed0ab7f849ead55dfac61cbaa0bea66
CPM <cpm.chuck@gmail.com>
2017-10-21 20:10:29 +0800

    ch1 - question complete

---
+++ ch1-review/src/practice/q5/Main.java
@@ -0,0 +1,12 @@
+package practice.q5;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String s = new String("3");
+		
+		//33456
+		System.out.println(1 + 2 + s + 4 + 5 + 6);
+	}
+
+}
