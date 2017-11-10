fd6b6be544752b90a405fe977215b1b0f6d100b6
CPM <cpm.chuck@gmail.com>
2017-11-11 00:57:10 +0800

    ch10 - file properties

--- ch10-NIO2/src/nio2File/channelAndIO/Main.java
+++ ch10-NIO2/src/nio2File/channelAndIO/Main.java
@@ -10,18 +10,32 @@ import java.nio.file.StandardOpenOption;
 
 public class Main {
 	
+	
+	/*
+	 * SeekableByteChannel -> ByteChannel -> Channel
+	 * 
+	 * SeekableByteChannel: random access
+	 * ByteChannel: provide base IO function
+	 * 
+	 * use SeekableByteChannel can implement random access data
+	 */
 	private static void testSeekableByteChannel() throws IOException {
 		Path path = Paths.get(System.getProperty("user.dir") + "\\file1.txt").toAbsolutePath();
+		
+		//1. open file
 		try(SeekableByteChannel sbc = Files.newByteChannel(path, StandardOpenOption.WRITE)) {
 			long channelSize = sbc.size();
+			
+			//find specific position, e.g, last word
 			sbc.position(channelSize);
 			System.out.println("position: " + sbc.position());
+			
+			//start to write
 			ByteBuffer buffer = ByteBuffer.wrap(("\n" + "0").getBytes());
 			sbc.write(buffer);
 			System.out.println("position: " + sbc.position());
 		}
 	}
-	
 	
 	public static void main(String[] args) throws IOException {
 		testSeekableByteChannel();
